b0e29dcc3e8780d955d97e21fc6e9d81231e9543
Anas Zari <azari@e2r6p11.1337.ma>
2023-11-17 11:49:37 +0100

    DAY 06 :: back to WORK

--- 42-CPP-06/ex00/ScalarConverter.cpp
+++ 42-CPP-06/ex00/ScalarConverter.cpp
@@ -6,31 +6,12 @@
 /*   By: azari <azari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 08:39:06 by azari             #+#    #+#             */
-/*   Updated: 2023/09/25 12:17:22 by azari            ###   ########.fr       */
+/*   Updated: 2023/11/17 11:49:04 by azari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter(){}
-ScalarConverter::~ScalarConverter(){}
-ScalarConverter::ScalarConverter(ScalarConverter& copy){
-    (void)copy;
+ScalarConverter::ScalarConverter(std::string litteral) : _litteral(litteral)
+{
 }
-ScalarConverter& ScalarConverter::operator=(ScalarConverter& other){
-    (void)other;
-    return *this;
-}
-
-std::string* toChar(double d){
-    
-    std::string *c = new std::string(1, static_cast<char>(d));
-    if (isprint((*c)[0]))
-        return c;
-    return 0;
-}
-
-// void ScalarConverter::convert(std::string litteral){
-    
-//     std::cout << "char : " << *get_char(strtod(litteral.c_str(), NULL)) << std::endl;
-// }
